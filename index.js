33ef7a381c751a83e1055aaa0de9667de9b7576c
Mari-8 <ryanaltimari8@gmail.com>
2021-03-17 12:49:11 -0500

    commit tree b6c951f9fcdb93615547d584b090f4c7995bdb1c

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(toDo="go to the office") {
+    return `This Monday, I will ${toDo}.`
+}   
+
+function wrapAdjective(flair="*") {
+    return function(adj="special") {
+        return `You are ${flair}${adj}${flair}!`
+    }
+   
+}
+
+function add() {
+    return 1 + 3
+}
+
+function subtract() {
+    return 1 - 3 
+}
+
+function multiply() {
+    return 1 * 3
+}
+
+function divide() {
+    return 10 / 5
+}
+
+const Calculator = {add, subtract, multiply, divide}; 
+
+
+function actionApplyer(int=0, arr) {
+    let start = int 
+
+    for (let i = 0; i < arr.length; i++ ){
+        start = arr[i](start)
+    }
+    return start
+}
